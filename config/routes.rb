fa31b71728749987fe5621b823a5917955278233
sergeband <andaomejob@gmail.com>
2013-06-20 13:31:48 +0200

    Done with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ AppSample::Application.routes.draw do
   get "pages/home"
 
   get "pages/contact"
+  
+  get "pages/about"
+  
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => 'welcome#index'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
